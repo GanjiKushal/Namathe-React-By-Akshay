a486064056ae55f0da9d3dfee89dae5d3b910af5
GanjiKushal <kushalganjiganji@gmail.com>
2023-07-18 01:18:23 +0530

    Using react components

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
 //JSX
-const heading = (
+const Title = () => (
   <h1 id="heading" className="head">
-    Namaste React from JSX
+    Namaste React from Title component
+  </h1>
+);
+
+
+const title =  (
+  <h1 id="heading" className="head">
+    Namaste React from title Element
   </h1>
 );
 
 //React Functional Component
-const HeadingComponent=()=>{
-  return(
-    <h1 id="heading">Namaste React from Functional Component</h1>
-  )
-}
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      {title}
+      {100+200}
+      <h1 id="heading">Namaste React from Functional Component</h1>
+    </div>
+  );
+};
 //const HeadingComponent2=()=><h1 id="heading">Namaste React from Functional Component</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(heading);
-root.render(HeadingComponent())
+root.render(<HeadingComponent />);
